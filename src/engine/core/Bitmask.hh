41dee73fe97f0847d546753a80d93a817eda3dc1
Armin Burgmeier <armin@arbur.net>
2021-05-13 22:22:58 +0200

    Add meson build files and unit tests for Bitmask

--- src/engine/core/Bitmask.hh
+++ src/engine/core/Bitmask.hh
@@ -1,25 +1,96 @@
 #pragma once
 
+#include <cassert>
+#include <cstdint>
+
 namespace freeisle {
 
+/**
+ * A helper class to store a bitmask of values of some type T,
+ * typically an enum. The enumerants must have numerical values
+ * between 0 and 31.
+ */
 template <typename T> class Bitmask {
 public:
-  Bitmask() = default;
-  Bitmask(T v) : val(1 << static_cast<uint32_t>(v)) {}
+  /**
+   * Default constructor, constructs a bitmask with no bits set.
+   */
+  Bitmask() : val(0) {}
 
+  /**
+   * Construct a bitmask with all the given bits set.
+   */
+  template <typename... U> Bitmask(T v, U... tail) : Bitmask(tail...) {
+    assert(static_cast<uint32_t>(v) < 8 * sizeof(uint32_t));
+    val |= (1 << static_cast<uint32_t>(v));
+  }
+
+  /**
+   * Computes the bitwise OR between two bitmasks.
+   */
   Bitmask<T> operator|(Bitmask<T> other) const {
     return Bitmask<T>(val | other.val);
   }
 
+  /**
+   * Computes the bitwise AND between two bitmasks.
+   */
   Bitmask<T> operator&(Bitmask<T> other) const {
     return Bitmask<T>(val & other.val);
   }
 
-  bool is_set(T bit) const { return (*this & bit).val != 0; }
-  void toggle(T bit) { val = val ^ bit; }
+  /**
+   * Computes the bitwise XOR between two bitmasks.
+   */
+  Bitmask<T> operator^(Bitmask<T> other) const {
+    return Bitmask<T>(val ^ other.val);
+  }
+
+  /**
+   * Compares two bitmasks for equality.
+   */
+  bool operator==(Bitmask<T> other) const { return val == other.val; }
+
+  /**
+   * Compares two bitmasks for inequality.
+   */
+  bool operator!=(Bitmask<T> other) const { return val != other.val; }
+
+  /**
+   * Returns whether the bit corresponding to the given value
+   * is set or not.
+   */
+  bool is_set(T bit) const { return are_all_set(bit); }
+
+  /**
+   * Toggles the best with the given value.
+   */
+  void toggle(T bit) { *this = *this ^ bit; }
+
+  /**
+   * Returns true if all bits in @param bits are set in the mask.
+   */
+  bool are_all_set(Bitmask<T> bits) const { return (*this & bits) == bits; }
+
+  /**
+   * Returns true if any of the bits in @param bits are set in the mask.
+   */
+  bool are_any_set(Bitmask<T> bits) const {
+    return (*this & bits) != Bitmask<T>{};
+  }
+
+  /**
+   * Returns true if there is at least one bit set in the mask.
+   */
+  bool are_any_set() const { return val != 0; }
+
+  /**
+   * Returns the number of bits set in the mask.
+   */
+  uint32_t n_set() const { return __builtin_popcount(val); }
 
 private:
-  explicit Bitmask<T>(uint32_t val) : val(val) {}
+  explicit Bitmask(uint32_t val) : val(val) {}
 
   uint32_t val;
 };
---
+++ src/engine/core/test/TestBitmask.cc
@@ -0,0 +1,104 @@
+#include "core/Bitmask.hh"
+
+#include <gtest/gtest.h>
+
+enum class TestEnum {
+  Test1,
+  Test2,
+  Test3,
+};
+
+TEST(Bitmask, DefaultCtor) {
+  freeisle::Bitmask<TestEnum> mask;
+  EXPECT_FALSE(mask.is_set(TestEnum::Test1));
+  EXPECT_FALSE(mask.is_set(TestEnum::Test2));
+  EXPECT_FALSE(mask.is_set(TestEnum::Test3));
+  EXPECT_FALSE(mask.are_any_set());
+  EXPECT_EQ(mask.n_set(), 0);
+}
+
+TEST(Bitmask, DefaultInit) {
+  freeisle::Bitmask<TestEnum> mask{};
+  EXPECT_FALSE(mask.is_set(TestEnum::Test1));
+  EXPECT_FALSE(mask.is_set(TestEnum::Test2));
+  EXPECT_FALSE(mask.is_set(TestEnum::Test3));
+  EXPECT_FALSE(mask.are_any_set());
+  EXPECT_EQ(mask.n_set(), 0);
+}
+
+TEST(Bitmask, SingleBit) {
+  freeisle::Bitmask<TestEnum> mask1(TestEnum::Test1);
+  EXPECT_TRUE(mask1.is_set(TestEnum::Test1));
+  EXPECT_FALSE(mask1.is_set(TestEnum::Test2));
+  EXPECT_FALSE(mask1.is_set(TestEnum::Test3));
+  EXPECT_TRUE(mask1.are_any_set());
+  EXPECT_EQ(mask1.n_set(), 1);
+
+  freeisle::Bitmask<TestEnum> mask2(TestEnum::Test2);
+  EXPECT_FALSE(mask2.is_set(TestEnum::Test1));
+  EXPECT_TRUE(mask2.is_set(TestEnum::Test2));
+  EXPECT_FALSE(mask2.is_set(TestEnum::Test3));
+  EXPECT_TRUE(mask2.are_any_set());
+  EXPECT_EQ(mask2.n_set(), 1);
+
+  freeisle::Bitmask<TestEnum> mask3(TestEnum::Test3);
+  EXPECT_FALSE(mask3.is_set(TestEnum::Test1));
+  EXPECT_FALSE(mask3.is_set(TestEnum::Test2));
+  EXPECT_TRUE(mask3.is_set(TestEnum::Test3));
+  EXPECT_TRUE(mask3.are_any_set());
+  EXPECT_EQ(mask3.n_set(), 1);
+}
+
+TEST(Bitmask, MultipleBits) {
+  freeisle::Bitmask<TestEnum> mask1(TestEnum::Test1, TestEnum::Test2);
+  EXPECT_TRUE(mask1.is_set(TestEnum::Test1));
+  EXPECT_TRUE(mask1.is_set(TestEnum::Test2));
+  EXPECT_FALSE(mask1.is_set(TestEnum::Test3));
+  EXPECT_TRUE(mask1.are_any_set());
+  EXPECT_EQ(mask1.n_set(), 2);
+
+  freeisle::Bitmask<TestEnum> mask2(TestEnum::Test1, TestEnum::Test3);
+  EXPECT_TRUE(mask2.is_set(TestEnum::Test1));
+  EXPECT_FALSE(mask2.is_set(TestEnum::Test2));
+  EXPECT_TRUE(mask2.is_set(TestEnum::Test3));
+  EXPECT_TRUE(mask2.are_any_set());
+  EXPECT_EQ(mask2.n_set(), 2);
+
+  freeisle::Bitmask<TestEnum> mask3 = mask1;
+
+  EXPECT_FALSE(mask1.are_all_set(mask2));
+  EXPECT_TRUE(mask1.are_all_set(mask3));
+  EXPECT_TRUE(mask1.are_all_set(TestEnum::Test1));
+  EXPECT_TRUE(mask1.are_all_set(TestEnum::Test2));
+  EXPECT_FALSE(mask1.are_all_set(TestEnum::Test3));
+  EXPECT_TRUE(mask1.are_any_set(mask2));
+  EXPECT_TRUE(mask1.are_any_set(mask3));
+  EXPECT_TRUE(mask1.are_any_set(TestEnum::Test1));
+  EXPECT_TRUE(mask1.are_any_set(TestEnum::Test2));
+  EXPECT_FALSE(mask1.are_any_set(TestEnum::Test3));
+}
+
+TEST(Bitmask, ToggleBits) {
+  freeisle::Bitmask<TestEnum> mask(TestEnum::Test1);
+
+  mask.toggle(TestEnum::Test3);
+  EXPECT_TRUE(mask.is_set(TestEnum::Test1));
+  EXPECT_FALSE(mask.is_set(TestEnum::Test2));
+  EXPECT_TRUE(mask.is_set(TestEnum::Test3));
+  EXPECT_TRUE(mask.are_any_set());
+  EXPECT_EQ(mask.n_set(), 2);
+
+  mask.toggle(TestEnum::Test1);
+  EXPECT_FALSE(mask.is_set(TestEnum::Test1));
+  EXPECT_FALSE(mask.is_set(TestEnum::Test2));
+  EXPECT_TRUE(mask.is_set(TestEnum::Test3));
+  EXPECT_TRUE(mask.are_any_set());
+  EXPECT_EQ(mask.n_set(), 1);
+
+  mask.toggle(TestEnum::Test3);
+  EXPECT_FALSE(mask.is_set(TestEnum::Test1));
+  EXPECT_FALSE(mask.is_set(TestEnum::Test2));
+  EXPECT_FALSE(mask.is_set(TestEnum::Test3));
+  EXPECT_FALSE(mask.are_any_set());
+  EXPECT_EQ(mask.n_set(), 0);
+}
