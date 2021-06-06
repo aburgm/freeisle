76940c0a6a6986bfa734ced3ec7e557b53669460
Armin Burgmeier <armin@arbur.net>
2021-06-07 00:20:24 +0200

    Implemnet serialization for unit definitions (#18)
    This is fairly straightforward with the primitives we have built earlier.
    The PR also contains a little bit of general cleanup.

---
+++ src/engine/state/serialize/UnitDefHandlers.cc
@@ -0,0 +1,136 @@
+#include "state/serialize/UnitDefHandlers.hh"
+
+#include "state/serialize/CollectionLoaders.hh"
+#include "state/serialize/CollectionSavers.hh"
+
+namespace freeisle::state::serialize {
+
+void ResupplyLoader::load(json::loader::Context &ctx, Json::Value &value) {
+  json::loader::EnumMapLoader<uint32_t, def::DamageType> ammo(
+      resupply_.ammo, def::DamageTypeNames);
+
+  resupply_.fuel = json::loader::load<uint32_t>(ctx, value, "fuel");
+  resupply_.repair = json::loader::load<uint32_t>(ctx, value, "repair");
+  json::loader::load_object(ctx, value, "ammo", ammo);
+}
+
+void ResupplySaver::save(json::saver::Context &ctx, Json::Value &value) {
+  json::saver::EnumMapSaver<uint32_t, def::DamageType> ammo(
+      resupply_.ammo, def::DamageTypeNames);
+
+  json::saver::save(ctx, value, "fuel", resupply_.fuel);
+  json::saver::save(ctx, value, "repair", resupply_.repair);
+  json::saver::save_object(ctx, value, "ammo", ammo);
+}
+
+void ContainerDefLoader::load(json::loader::Context &ctx, Json::Value &value) {
+  container_.max_units = json::loader::load<uint32_t>(ctx, value, "max_units");
+  container_.max_weight =
+      json::loader::load<uint32_t>(ctx, value, "max_weight");
+  container_.supported_levels = json::loader::load_bitmask<def::Level>(
+      ctx, value, "supported_levels", def::Levels);
+}
+
+void ContainerDefSaver::save(json::saver::Context &ctx, Json::Value &value) {
+  json::saver::save(ctx, value, "max_units", container_.max_units);
+  json::saver::save(ctx, value, "max_weight", container_.max_weight);
+  json::saver::save_bitmask(ctx, value, "supported_levels",
+                            container_.supported_levels, def::Levels);
+}
+
+void WeaponDefLoader::load(json::loader::Context &ctx, Json::Value &value) {
+  assert(weapon_ != nullptr);
+
+  weapon_->name = json::loader::load<std::string>(ctx, value, "name");
+  weapon_->damage_type = json::loader::load_enum<def::DamageType>(
+      ctx, value, "damage_type", def::DamageTypes);
+  weapon_->damage = json::loader::load<uint32_t>(ctx, value, "damage");
+  weapon_->min_range = json::loader::load<uint32_t>(ctx, value, "min_range");
+  weapon_->max_range = json::loader::load<uint32_t>(ctx, value, "max_range");
+  weapon_->ammo = json::loader::load<uint32_t>(ctx, value, "ammo");
+}
+
+void WeaponDefSaver::save(json::saver::Context &ctx, Json::Value &value) {
+  assert(weapon_ != nullptr);
+
+  json::saver::save(ctx, value, "name", weapon_->name);
+  json::saver::save_enum(ctx, value, "damage_type", weapon_->damage_type,
+                         def::DamageTypes);
+  json::saver::save(ctx, value, "damage", weapon_->damage);
+  json::saver::save(ctx, value, "min_range", weapon_->min_range);
+  json::saver::save(ctx, value, "max_range", weapon_->max_range);
+  json::saver::save(ctx, value, "ammo", weapon_->ammo);
+}
+
+void UnitDefLoader::load(json::loader::Context &ctx, Json::Value &value) {
+  assert(unit_ != nullptr);
+
+  json::loader::EnumMapLoader<uint32_t, def::BaseTerrainType,
+                              def::OverlayTerrainType>
+      movement_cost(unit_->movement_cost, def::TerrainTypeNames);
+  json::loader::EnumMapLoader<uint32_t, def::BaseTerrainType,
+                              def::OverlayTerrainType>
+      protection(unit_->protection, def::TerrainTypeNames);
+  json::loader::EnumMapLoader<uint32_t, def::DamageType> resistance(
+      unit_->resistance, def::DamageTypeNames);
+  ResupplyLoader supplies(unit_->supplies);
+  CollectionLoader<def::WeaponDef, WeaponDefLoader> weapons(unit_->weapons);
+  ContainerDefLoader container(unit_->container);
+
+  unit_->name = json::loader::load<std::string>(ctx, value, "name");
+  unit_->description =
+      json::loader::load<std::string>(ctx, value, "description");
+  unit_->level = json::loader::load_enum(ctx, value, "level", def::Levels);
+  unit_->caps =
+      json::loader::load_bitmask(ctx, value, "caps", def::UnitDefCaps);
+  unit_->armor = json::loader::load<uint32_t>(ctx, value, "armor");
+  unit_->movement = json::loader::load<uint32_t>(ctx, value, "movement");
+  unit_->fuel = json::loader::load<uint32_t>(ctx, value, "fuel");
+  unit_->weight = json::loader::load<uint32_t>(ctx, value, "weight");
+  json::loader::load_object(ctx, value, "movement_cost", movement_cost);
+  json::loader::load_object(ctx, value, "protection", protection);
+  json::loader::load_object(ctx, value, "resistance", resistance);
+  json::loader::load_object(ctx, value, "supplies", supplies);
+  json::loader::load_object(ctx, value, "weapons", weapons);
+  json::loader::load_object(ctx, value, "container", container);
+  unit_->value = json::loader::load<uint32_t>(ctx, value, "value");
+  unit_->view_range = json::loader::load<uint32_t>(ctx, value, "view_range");
+  unit_->jamming_range =
+      json::loader::load<uint32_t>(ctx, value, "jamming_range");
+}
+
+void UnitDefSaver::save(json::saver::Context &ctx, Json::Value &value) {
+  assert(unit_ != nullptr);
+
+  json::saver::EnumMapSaver<uint32_t, def::BaseTerrainType,
+                            def::OverlayTerrainType>
+      movement_cost(unit_->movement_cost, def::TerrainTypeNames);
+  json::saver::EnumMapSaver<uint32_t, def::BaseTerrainType,
+                            def::OverlayTerrainType>
+      protection(unit_->protection, def::TerrainTypeNames);
+  json::saver::EnumMapSaver<uint32_t, def::DamageType> resistance(
+      unit_->resistance, def::DamageTypeNames);
+  ResupplySaver supplies(unit_->supplies);
+  CollectionSaver<def::WeaponDef, WeaponDefSaver> weapons(unit_->weapons);
+  ContainerDefSaver container(unit_->container);
+
+  json::saver::save(ctx, value, "name", unit_->name);
+  json::saver::save(ctx, value, "description", unit_->description);
+  json::saver::save_enum(ctx, value, "level", unit_->level, def::Levels);
+  json::saver::save_bitmask(ctx, value, "caps", unit_->caps, def::UnitDefCaps);
+  json::saver::save(ctx, value, "armor", unit_->armor);
+  json::saver::save(ctx, value, "movement", unit_->movement);
+  json::saver::save(ctx, value, "fuel", unit_->fuel);
+  json::saver::save(ctx, value, "weight", unit_->weight);
+  json::saver::save_object(ctx, value, "movement_cost", movement_cost);
+  json::saver::save_object(ctx, value, "protection", protection);
+  json::saver::save_object(ctx, value, "resistance", resistance);
+  json::saver::save_object(ctx, value, "supplies", supplies);
+  json::saver::save_object(ctx, value, "weapons", weapons);
+  json::saver::save_object(ctx, value, "container", container);
+  json::saver::save(ctx, value, "value", unit_->value);
+  json::saver::save(ctx, value, "view_range", unit_->view_range);
+  json::saver::save(ctx, value, "jamming_range", unit_->jamming_range);
+}
+
+} // namespace freeisle::state::serialize
---
+++ src/engine/state/serialize/UnitDefHandlers.hh
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "state/serialize/AuxData.hh"
+
+#include "json/LoadUtil.hh"
+#include "json/SaveUtil.hh"
+
+#include "def/UnitDef.hh"
+
+namespace freeisle::state::serialize {
+
+class ResupplyLoader {
+public:
+  ResupplyLoader(def::Resupply &resupply) : resupply_(resupply) {}
+
+  void load(json::loader::Context &ctx, Json::Value &value);
+
+private:
+  def::Resupply &resupply_;
+};
+
+class ResupplySaver {
+public:
+  ResupplySaver(const def::Resupply &resupply) : resupply_(resupply) {}
+
+  void save(json::saver::Context &ctx, Json::Value &value);
+
+private:
+  const def::Resupply &resupply_;
+};
+
+class ContainerDefLoader {
+public:
+  ContainerDefLoader(def::ContainerDef &container) : container_(container) {}
+
+  void load(json::loader::Context &ctx, Json::Value &value);
+
+private:
+  def::ContainerDef &container_;
+};
+
+class ContainerDefSaver {
+public:
+  ContainerDefSaver(const def::ContainerDef &container)
+      : container_(container) {}
+
+  void save(json::saver::Context &ctx, Json::Value &value);
+
+private:
+  const def::ContainerDef &container_;
+};
+
+class WeaponDefLoader {
+public:
+  WeaponDefLoader() : weapon_(nullptr) {}
+
+  void set(def::WeaponDef &def) { weapon_ = &def; }
+  void load(json::loader::Context &ctx, Json::Value &value);
+
+private:
+  def::WeaponDef *weapon_;
+};
+
+class WeaponDefSaver {
+public:
+  WeaponDefSaver() : weapon_(nullptr) {}
+
+  void set(const def::WeaponDef &def) { weapon_ = &def; }
+  void save(json::saver::Context &ctx, Json::Value &value);
+
+private:
+  const def::WeaponDef *weapon_;
+};
+
+class UnitDefLoader {
+public:
+  UnitDefLoader(AuxData &aux) : aux_(aux), unit_(nullptr) {}
+
+  void set(def::UnitDef &unit) { unit_ = &unit; }
+  void load(json::loader::Context &ctx, Json::Value &value);
+
+private:
+  AuxData &aux_;
+  def::UnitDef *unit_;
+};
+
+class UnitDefSaver {
+public:
+  UnitDefSaver(AuxData &aux) : aux_(aux), unit_(nullptr) {}
+
+  void set(const def::UnitDef &unit) { unit_ = &unit; }
+  void save(json::saver::Context &ctx, Json::Value &value);
+
+private:
+  AuxData &aux_;
+  const def::UnitDef *unit_;
+};
+
+} // namespace freeisle::state::serialize
